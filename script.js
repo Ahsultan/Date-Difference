632c4b5a3f29bc983aab93f8af4717c2f555d98c
sultan ahmed <65736680+jeysul@users.noreply.github.com>
2022-04-10 22:27:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//Declaring and initializing variables
+let submit = document.getElementById("submit");
+let output = document.getElementById("output");
+let month = document.getElementById("month");
+let years = document.getElementById("years");
+
+submit.addEventListener("click", () => {
+  //Create a Date object from input value
+  let date1 = new Date(document.getElementById("date-1").value);
+  let date2 = new Date(document.getElementById("date-2").value);
+
+  //Check if the input dates are valid
+  //If valid calculate the difference
+  if (date1.getTime() && date2.getTime()) {
+    //Calculate difference in time using getTime function
+    //getTime calculates number of years since January 1,1970
+    let timeDifference = date2.getTime() - date1.getTime();
+
+    //Since this value is in milliseconds we need to convert it into days
+    //We want the difference to be a non-negative number. Hence we use Math.abs()
+    let dayDifference = Math.abs(timeDifference / (1000 * 3600 * 24));
+    let mon = Math.abs(Math.round(timeDifference/(1000 * 60*60*24*7*4)));
+    let year = Math.abs(Math.round(timeDifference/(1000 * 60 * 60 * 24)) / 365);
+    output.innerHTML = `Difference between the two dates is <span>${dayDifference}</span> days`;
+    month.innerHTML = `Difference between the two dates in Months is <span>${mon}</span> months`;
+    years.innerHTML = `Difference between the two dates in Years is <span>${year}</span> year`;
+  }
+
+  //Else display that the input is valid
+  else {
+    output.innerHTML = "Please select a valid date";
+  }
+});
